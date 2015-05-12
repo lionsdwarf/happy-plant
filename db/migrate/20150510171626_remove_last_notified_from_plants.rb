b68d53b985fc4179071737b90613bfcb9a6a8ad5
Joe Leinsdorf <leinsdorf@gmail.com>
2015-05-12 11:27:23 -0400

    heroku db problems

--- db/migrate/20150510171626_remove_last_notified_from_plants.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLastNotifiedFromPlants < ActiveRecord::Migration
-  def change
-    remove_column :plants, :last_notified, :timestamp
-  end
-end
--- db/migrate/20150512125249_fix_column_name.rb
+++
@@ -1,5 +0,0 @@
-class FixColumnName < ActiveRecord::Migration
-  def change
-    rename_column :users, :phone_num, :phone_num1
-  end
-end
